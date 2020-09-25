fde7e8a7a8c065af7560d67de435c6ee1b53f8ae
Anushka Agrawal <anushkaagrawal280@gmail.com>
2020-09-25 20:27:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
 
-@app.route('/<name>')
-def index(name):
-	return '<h1>HELLO {}!</h1>'.format(name)
+@app.route('/')
+def index():
+	return '<h1>HELLO WORLD!</h1>'
+
+@app.route('/home/<name>', methods=['GET'])
+def home(name):
+	return '<h1>You {} are on the home page!</h1>'.format(name)
+
+@app.route('/json')
+def json():
+	return jsonify({'key' : 'value', 'listkey' : [1,2,3]})
+
+if __name__=='__main__':
+	app.run(debug=True)
